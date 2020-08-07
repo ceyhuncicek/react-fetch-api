21a3495a26d41632e686972a1844d04430350e2e
Ceyhun <40377224+1Ceko@users.noreply.github.com>
2020-08-07 22:14:41 +0200

    api added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [result, setResult] = useState();
+  const [search, setSearch] = useState("vodka");
+
+  // this useEffect will run once
+  // similar to componentDidMount()
+  useEffect(() => {
+    fetch(`https://the-cocktail-db.p.rapidapi.com/search.php?i=${search}`, {
+      method: "GET",
+      headers: {
+        "x-rapidapi-host": "the-cocktail-db.p.rapidapi.com",
+        "x-rapidapi-key": "4e5a9c6606msh0502fd0ee10632fp1394b0jsna7cfb4ba39c1"
+      }
+    })
+      .then(res => res.json())
+      .then(
+        result => {
+          setIsLoaded(true);
+          setResult(result);
+          console.log(search);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        error => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
+  });
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <ul>
+        {console.log(result)}
+        <div className="resultContainer">
+          {result?.ingredients?.map(ingredients => (
+            <div className="result__listItem" key={ingredients?.idIngredient}>
+              <p>
+                <b>Name:</b>
+                {ingredients?.strIngredient}
+              </p>
+              <p>
+                <b>Description: </b>
+                {ingredients?.strDescription}
+              </p>
+            </div>
+          ))}
+        </div>
+      </ul>
+    );
+  }
 }
 
 export default App;
